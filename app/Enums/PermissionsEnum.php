f500c36fe8c14af9dc26f44792c1568ddf594c43
Edgar Vergara <edgarvm13@hotmail.com>
2025-01-06 12:44:21 -0800

    ✨ feat: add RolesEnum and PermissionsEnum for role-based access control; implement RoleSeeder and UserSeeder for initial data population

---
+++ app/Enums/PermissionsEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum PermissionsEnum: string
+{
+    //
+    case ApproveVendors = "ApproveVendors";
+    case SellProducts = "SellProducts";
+    case BuyProducts = "BuyProducts";
+}
---
+++ app/Enums/RolesEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum RolesEnum: string
+{
+    //
+    case Admin = "Admin";
+    case Vendor = "Vendor";
+    case User = "User";
+}
