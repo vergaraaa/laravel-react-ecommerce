687341bd32cafc08c87a13ac4abe0edc3b604bec
Edgar Vergara <edgarvm13@hotmail.com>
2025-01-06 11:48:32 -0800

    ✨ feat: update Navbar and Login components for user authentication; integrate AuthenticatedLayout

--- resources/js/Components/App/Navbar.tsx
+++ resources/js/Components/App/Navbar.tsx
@@ -1,6 +1,9 @@
-import { Link } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
 
 export const Navbar = () => {
+  const { auth } = usePage().props;
+  const { user } = auth;
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -9,7 +12,7 @@ export const Navbar = () => {
         </Link>
       </div>
 
-      <div className="flex-none">
+      <div className="flex-none gap-4">
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <div className="indicator">
@@ -43,36 +46,52 @@ export const Navbar = () => {
             </div>
           </div>
         </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link href={route('profile.edit')} className="justify-between">
-                Profile
-              </Link>
-            </li>
 
-            <li>
-              <Link href={route('logout')} method="post" as="button">
-                Logout
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/* DROPDOWN FOR USER */}
+        {user && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link href={route('profile.edit')} className="justify-between">
+                  Profile
+                </Link>
+              </li>
+
+              <li>
+                <Link href={route('logout')} method="post" as="button">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {/* LOGIN AND REGISTER LINKS */}
+        {!user && (
+          <>
+            <Link href={route('login')} className="btn">
+              Login
+            </Link>
+            <Link href={route('register')} className="btn btn-primary">
+              Register
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
