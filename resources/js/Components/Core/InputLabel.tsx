c354b19d48437382878dd314311b41a14ea5a4a3
Edgar Vergara <edgarvm13@hotmail.com>
2025-01-06 07:50:36 -0800

    ♻️ refactor: update component styles and structure for consistency; add daisyui dependency

--- resources/js/Components/Core/InputLabel.tsx
+++ resources/js/Components/Core/InputLabel.tsx
@@ -1,20 +1,14 @@
 import { LabelHTMLAttributes } from 'react';
 
 export default function InputLabel({
-    value,
-    className = '',
-    children,
-    ...props
+  value,
+  className = '',
+  children,
+  ...props
 }: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
-    return (
-        <label
-            {...props}
-            className={
-                `block text-sm font-medium text-gray-700 dark:text-gray-300 ` +
-                className
-            }
-        >
-            {value ? value : children}
-        </label>
-    );
+  return (
+    <label {...props} className={`label` + className}>
+      <span className="label-text">{value ? value : children}</span>
+    </label>
+  );
 }
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -85,10 +85,7 @@ export default function Login({
 
         <div className="mt-4 flex items-center justify-end">
           {canResetPassword && (
-            <Link
-              href={route('password.request')}
-              className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-            >
+            <Link href={route('password.request')} className="link">
               Forgot your password?
             </Link>
           )}
