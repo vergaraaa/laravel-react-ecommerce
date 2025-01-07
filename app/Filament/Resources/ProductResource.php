7d277ea313343db5b53a1b3ee06d4e2767ceeb00
Edgar Vergara <edgarvm13@hotmail.com>
2025-01-07 13:39:01 -0800

    ✨ feat: add Product model, resource, and status enum; implement CRUD pages and migration for product management

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\ProductStatusEnum;
+use App\Enums\RolesEnum;
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Grid::make()
+                    ->schema([
+                        TextInput::make('title')
+                            ->live(onBlur: true)
+                            ->required()
+                            ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                                $set("slug", Str::slug($state));
+                            }),
+                        TextInput::make('slug')
+                            ->required(),
+                        Select::make('department_id')
+                            ->relationship('department', 'name')
+                            ->label(__('Department'))
+                            ->preload()
+                            ->searchable()
+                            ->required()
+                            ->reactive() // makes field reactive to changes
+                            ->afterStateUpdated(function (callable $set) {
+                                $set('category_id', null); // reset category when department changes
+                            }),
+                        Select::make('category_id')
+                            ->relationship(
+                                'category',
+                                'name',
+                                function (Builder $query, callable $get) {
+                                    $departmentId = $get('department_id');
+                                    if ($departmentId) {
+                                        $query->where('department_id', $departmentId); // filter categories based on departments
+                                    }
+                                }
+                            )
+                            ->label(__('Category'))
+                            ->preload()
+                            ->searchable()
+                            ->required()
+                    ]),
+                Forms\Components\RichEditor::make('description')
+                    ->required()
+                    ->toolbarButtons([
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderedList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                        '†able',
+                    ])
+                    ->columnSpan(2),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('quantity')
+                    ->integer(),
+                Select::make('status')
+                    ->options(ProductStatusEnum::labels())
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("title")
+                    ->sortable()
+                    ->words(10)
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->badge()
+                    ->colors(ProductStatusEnum::colors()),
+                TextColumn::make('department.name'),
+                TextColumn::make('category.name'),
+                TextColumn::make('created_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->options(ProductStatusEnum::labels()),
+                SelectFilter::make('department_id')
+                    ->relationship('department', 'name')
+                    ->label('Department'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function canViewAny(): bool
+    {
+        /** @var User $user */
+        $user = Filament::auth()->user();
+
+        return $user && $user->hasRole(RolesEnum::Vendor);
+    }
+}
---
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
+
+class CreateProduct extends CreateRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = Auth::user()->id;
+        $data['updated_by'] = Auth::user()->id;
+
+        return $data;
+    }
+}
